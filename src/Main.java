343cac487c8c0bfcc53781ba4426d2e865da66e5
gl_sokolov <gl_sokolov@mail.ru>
2022-09-25 21:29:03 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var dog = 8;
+        dog = dog + 4;
+        System.out.println(dog - 3.5);
+        var cat = 3.6;
+        cat = cat + 4;
+        System.out.println(cat - 1.6);
+        var paper = 763789;
+        paper = paper + 4;
+        System.out.println(paper - 7639);
     }
 }
